520cd4d2c542e13679cf164b089a84006166ad83
wessboy <hedhibioussama@gmail.com>
2024-02-02 15:55:00 +0100

    Add project files.

---
+++ SignalR-realtime-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_realtime_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
